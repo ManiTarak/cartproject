7ace29a9c86bf6c08b3d77811866919698f5c6ac
mani_tarak <manikumarjammula999@gmail.com>
2023-04-06 18:04:40 +0530

    reading data from firebase

--- src/App.js
+++ src/App.js
@@ -3,32 +3,39 @@ import React from "react";
 import Cart from './Cart.js';
 import Navbar from './NavBar.js';
 import Footer from "./Footer.js";
+//importing firebase
+import firebase from 'firebase/compat/app';
 
 class App extends React.Component{
+   
   constructor(){
     super();
+    
     this.state={
-        products:[{
-            title:"Watch",
-            price:99,
-            qty:11,
-            img:"https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcTixQF0-4NW-7i8nJidCWpEEmSbVKB2lCoDP_28I03h3HKYHZh-JWBJydy96-MW_NFQQhCzt_WCxWEo082fCGgQ4Gz9ggdfjTzB3atiNGFO61hi0L6gBL69",
-            id:1
-        },
-        {
-            title:"Phone",
-            price:9999,
-            qty:1,
-            img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4DMB5fDz2ESb4aVOzw4TujQYX1k_XE98AgQ&usqp=CAU",
-            id:2
-        },
-        {
-            title:"Mac",
-            price:99999,
-            qty:1,
-            img:"https://upload.wikimedia.org/wikipedia/commons/7/76/MacBook-Pro-13inch.jpg",
-            id:3
-        }],
+      loading : true,
+         products:[//{
+        //     title:"Watch",
+        //     price:99,
+        //     qty:11,
+        //     img:"https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcTixQF0-4NW-7i8nJidCWpEEmSbVKB2lCoDP_28I03h3HKYHZh-JWBJydy96-MW_NFQQhCzt_WCxWEo082fCGgQ4Gz9ggdfjTzB3atiNGFO61hi0L6gBL69",
+        //     id:1
+        // },
+        // {
+        //     title:"Phone",
+        //     price:9999,
+        //     qty:1,
+        //     img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4DMB5fDz2ESb4aVOzw4TujQYX1k_XE98AgQ&usqp=CAU",
+        //     id:2
+        // },
+        // {
+        //     title:"Mac",
+        //     price:99999,
+        //     qty:1,
+        //     img:"https://upload.wikimedia.org/wikipedia/commons/7/76/MacBook-Pro-13inch.jpg",
+        //     id:3
+        // }
+      ],
+    
     
 
 handleIncreaseQuantity : (product)=>{
@@ -60,6 +67,31 @@ handleDecreaseQuantity : (product)=>{
  }
 }
 }
+componentDidMount(){
+  // getting data from firebase 
+  firebase
+  .firestore()
+  .collection('Products')
+  .get()
+  .then((snapshot)=>{
+    const products=(snapshot.docs.map((doc)=>{
+      const d = doc.data();
+      d.id=doc.id;
+      return d;
+    }));
+    
+    this.setState((prevState)=>{
+   return {
+      products:products,
+      loading:false
+    }});
+
+  })
+
+
+  
+ 
+}
 getCartCount = () => {
     const {products}=this.state;
     var count=0;
@@ -79,7 +111,12 @@ getTotalPrice =() =>{
 }
 
   render(){
+    const t=true;
     const {products,handleIncreaseQuantity,handleDecreaseQuantity,handleDeleteCartItem}=this.state;
+    //initial to show Loading 
+    if(this.state.loading){
+      return (<h1>Loading Data..........</h1>);
+    }
   return (
     <div className="App">
       <Navbar count={this.getCartCount()}/>
--- src/index.js
+++ src/index.js
@@ -2,7 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import firebase from 'firebase/compat/app';
 
+import 'firebase/compat/firestore';
+const firebaseConfig = {
+  apiKey: "AIzaSyCe4bpI7MIRN-heK3D9z1FtfN2qw-wxTyM",
+  authDomain: "cart-app-d9c12.firebaseapp.com",
+  projectId: "cart-app-d9c12",
+  storageBucket: "cart-app-d9c12.appspot.com",
+  messagingSenderId: "100496702622",
+  appId: "1:100496702622:web:d2176a1e7e74d54b1c0f8f"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
